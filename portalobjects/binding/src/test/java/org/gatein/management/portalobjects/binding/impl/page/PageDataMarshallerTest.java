2254f0473e99254e500844807c207c6ed26f256c
nscavell <nick.scavelli@gmail.com>
2011-01-21 17:15:36 -0500

    Unit test for page marshaller

--- portalobjects/binding/src/test/java/org/gatein/management/portalobjects/binding/impl/page/PageDataMarshallerTest.java
+++ portalobjects/binding/src/test/java/org/gatein/management/portalobjects/binding/impl/page/PageDataMarshallerTest.java
@@ -1,23 +1,44 @@
+/*
+ * JBoss, a division of Red Hat
+ * Copyright 2011, Red Hat Middleware, LLC, and individual
+ * contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package org.gatein.management.portalobjects.binding.impl.page;
 
+import org.exoplatform.portal.config.model.ApplicationState;
 import org.exoplatform.portal.config.model.ApplicationType;
 import org.exoplatform.portal.config.model.TransientApplicationState;
+import org.exoplatform.portal.pom.config.Utils;
 import org.exoplatform.portal.pom.data.ApplicationData;
 import org.exoplatform.portal.pom.data.ComponentData;
+import org.exoplatform.portal.pom.data.ContainerData;
 import org.exoplatform.portal.pom.data.PageData;
 import org.exoplatform.portal.pom.spi.portlet.Portlet;
-import org.exoplatform.portal.pom.spi.portlet.PortletBuilder;
-import org.junit.Assert;
-import org.junit.Before;
+import org.exoplatform.portal.pom.spi.portlet.Preference;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.awt.*;
 import java.util.List;
-import java.util.Map;
+
+import static org.junit.Assert.*;
 
 /**
  * @author <a href="mailto:nscavell@redhat.com">Nick Scavelli</a>
@@ -25,75 +46,248 @@ import java.util.Map;
  */
 public class PageDataMarshallerTest
 {
-   private PageData customPageData;
-   private PageData homePage;
-
-   @Before
-   public void createData()
+   @Test
+   public void testHomePageMarshalling()
    {
-      // Create custom page data
-      Portlet portlet = new PortletBuilder().add("pref-name1", "pref-value1").add("pref-name2", "pref-value2").build();
-      TransientApplicationState<Portlet> state = new TransientApplicationState<Portlet>("app-ref/portlet-ref", portlet);
+      PageDataMarshaller marshaller = new PageDataMarshaller();
+      PageData page = marshaller.unmarshal(getClass().getResourceAsStream("/home-page.xml"));
+      assertNotNull(page);
+      assertEquals("homepage", page.getName());
+      assertEquals("Home Page", page.getTitle());
+      assertEquals("Everyone", Utils.join(";", page.getAccessPermissions()));
+      assertEquals("*:/platform/administrators", page.getEditPermission());
+      assertNotNull(page.getChildren());
+      assertEquals(1, page.getChildren().size());
+      ComponentData component = page.getChildren().get(0);
+      assertTrue(component instanceof ApplicationData);
+      @SuppressWarnings("unchecked")
+      ApplicationData<Portlet> application = (ApplicationData<Portlet>) component;
+      assertTrue(application.getType() == ApplicationType.PORTLET);
+      ApplicationState<Portlet> state = application.getState();
+      assertNotNull(state);
+      assertTrue(state instanceof TransientApplicationState);
+      TransientApplicationState<Portlet> tas = (TransientApplicationState<Portlet>) state;
+      assertEquals("web/HomePagePortlet", tas.getContentId());
+      Portlet portlet = tas.getContentState();
+      int count = 0;
+      for (Preference pref : portlet)
+      {
+         count++;
+      }
+      assertEquals(1, count);
+      Preference pref = portlet.getPreference("template");
+      assertNotNull(pref);
+      assertEquals("template", pref.getName());
+      assertEquals("system:/templates/groovy/webui/component/UIHomePagePortlet.gtmpl", pref.getValue());
+      assertFalse(pref.isReadOnly());
 
-      Map<String,String> properties = new HashMap<String,String>();
-      properties.put("key1", "value1");
-      properties.put("key2", "value2");
-
-      ApplicationData<Portlet> applicationData = new ApplicationData<Portlet>("app-storageId", "app-storageName",
-         ApplicationType.PORTLET, state, "app-id", "app-title", "app-icon", "app-description", true, true, false,
-         "app-theme", "app-width", "app-height", properties, Arrays.asList("app-/platform/users", "app-/platform/administrators"));
-
-      List<ComponentData> children = new ArrayList<ComponentData>();
-      children.add(applicationData);
-      customPageData = new PageData("storageId", "id", "custom-page-name", "icon", "template", "factoryId", "title", "description",
-         "width", "height", Arrays.asList("/platform/users", "/platform/administrators"),
-         children, "ownerType", "ownerId", "editPermission", true);
-
-
-      // Create out of the box home page
-      portlet = new PortletBuilder().add("template", "system:/templates/groovy/webui/component/UIHomePagePortlet.gtmpl").build();
-      state = new TransientApplicationState<Portlet>("web/HomePagePortlet", portlet);
-
-      applicationData = new ApplicationData<Portlet>(null, null,
-         ApplicationType.PORTLET, state, null, "Home Page portlet", null, null, false, false, false,
-         null, null, null, null, Arrays.asList("Everyone"));
-
-      children = new ArrayList<ComponentData>();
-      children.add(applicationData);
-      
-      homePage = new PageData(null, null, "homepage", null, null, null, "Home Page", null,
-         null, null, Arrays.asList("Everyone"),
-         children, "", "", "*:/platform/administrators", false);
+      assertNull(application.getTheme());
+      assertEquals("Home Page portlet", application.getTitle());
+      assertEquals("Everyone", Utils.join(";", application.getAccessPermissions()));
+      assertFalse(application.isShowInfoBar());
+      assertFalse(application.isShowApplicationState());
+      assertFalse(application.isShowApplicationMode());
+      assertNull(application.getDescription());
+      assertNull(application.getIcon());
+      assertNull(application.getWidth());
+      assertNull(application.getHeight());
    }
 
    @Test
-   public void testCustomPageDataMarshaller() throws Exception
+   public void testLoadedPage()
    {
       PageDataMarshaller marshaller = new PageDataMarshaller();
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      PageData page = marshaller.unmarshal(getClass().getResourceAsStream("/loaded-page.xml"));
+      assertNotNull(page);
 
-      marshaller.marshal(customPageData, baos);
+      // Verify page properties
+      assertEquals("loaded-page", page.getName());
+      assertEquals("Loaded Page", page.getTitle());
+      assertEquals("manager:/platform/administrators;manager:/platform/users", Utils.join(";", page.getAccessPermissions()));
+      assertEquals("*:/platform/administrators", page.getEditPermission());
 
-      baos.flush();
-      baos.close();
+      // Verify page children
+      assertNotNull(page.getChildren());
+      assertEquals(1, page.getChildren().size());
+      ComponentData component = page.getChildren().get(0);
+      assertTrue(component instanceof ContainerData);
 
-      ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+      // Verify root container
+      ContainerData rootContainer = (ContainerData) component;
+      assertEquals(rootContainer.getTemplate(), "system:/groovy/portal/webui/container/UIContainer.gtmpl");
+      assertEquals("Everyone", Utils.join(";", rootContainer.getAccessPermissions()));
 
-      PageData customPage = marshaller.unmarshal(bais);
+      // Verify root container children
+      List<ComponentData> rootChildren = rootContainer.getChildren();
+      assertNotNull(rootChildren);
+      assertEquals(3, rootChildren.size());
 
-      Assert.assertNotNull(customPage);
-      Assert.assertEquals("custom-page-name", customPage.getName());
+      // Verify container 1
+      ComponentData c1 = rootChildren.get(0);
+      assertNotNull(c1);
+      assertTrue(c1 instanceof ContainerData);
+      ContainerData container1 = (ContainerData) c1;
+      assertEquals(container1.getTemplate(), "system:/groovy/portal/webui/container/UIContainer.gtmpl");
+      assertEquals("*:/platform/users", Utils.join(";", container1.getAccessPermissions()));
+      {
+         // Verify homepage application
+         assertNotNull(container1.getChildren());
+         assertEquals(1, container1.getChildren().size());
+         ComponentData homeComponent = container1.getChildren().get(0);
+         assertTrue(homeComponent instanceof ApplicationData);
+         @SuppressWarnings("unchecked")
+         ApplicationData<Portlet> application = (ApplicationData<Portlet>) homeComponent;
+         assertTrue(application.getType() == ApplicationType.PORTLET);
+         ApplicationState<Portlet> state = application.getState();
+         assertNotNull(state);
+         assertTrue(state instanceof TransientApplicationState);
+         TransientApplicationState<Portlet> tas = (TransientApplicationState<Portlet>) state;
+         assertEquals("web/HomePagePortlet", tas.getContentId());
+         Portlet portlet = tas.getContentState();
+         int count = 0;
+         for (Preference pref : portlet)
+         {
+            count++;
+         }
+         assertEquals(1, count);
+         Preference pref = portlet.getPreference("template");
+         assertNotNull(pref);
+         assertEquals("template", pref.getName());
+         assertEquals("system:/templates/groovy/webui/component/UIHomePagePortlet.gtmpl", pref.getValue());
+         assertTrue(pref.isReadOnly());
 
-      Assert.assertNotNull(customPage.getChildren());
-      Assert.assertEquals(1, customPage.getChildren().size());
+         assertEquals("Mac:MacTheme::Default:DefaultTheme::Vista:VistaTheme", application.getTheme());
+         assertEquals("Home Page portlet", application.getTitle());
+         assertEquals("Everyone", Utils.join(";", application.getAccessPermissions()));
+         assertTrue(application.isShowInfoBar());
+         assertTrue(application.isShowApplicationState());
+         assertTrue(application.isShowApplicationMode());
+         assertNull(application.getDescription());
+         assertNull(application.getIcon());
+         assertNull(application.getWidth());
+         assertNull(application.getHeight());
+      }
 
-      Assert.assertTrue(customPage.getChildren().get(0) instanceof ApplicationData);
-      ApplicationData applicationData = (ApplicationData) customPage.getChildren().get(0);
-      Assert.assertTrue(applicationData.getState() instanceof TransientApplicationState);
-      TransientApplicationState state = (TransientApplicationState) applicationData.getState();
-      Assert.assertEquals("app-ref/portlet-ref", state.getContentId());
-      Assert.assertTrue(state.getContentState() instanceof Portlet);
+      // Verify container 2
+      ComponentData c2 = rootChildren.get(1);
+      assertNotNull(c2);
+      assertTrue(c2 instanceof ContainerData);
+      ContainerData container2 = (ContainerData) c2;
+      assertEquals(container2.getTemplate(), "system:/groovy/portal/webui/container/UITableColumnContainer.gtmpl");
+      assertEquals("*:/platform/guests", Utils.join(";", container2.getAccessPermissions()));
+      assertEquals("TableColumnContainer", container2.getFactoryId());
+      assertNotNull(container2.getChildren());
+      assertEquals(2, container2.getChildren().size());
 
-      //TODO: Test all properties
+      {
+         // Verify column 1 of container 2
+         ComponentData appregComp = container2.getChildren().get(0);
+         assertTrue(appregComp instanceof ContainerData);
+         ContainerData appregContainer = (ContainerData) appregComp;
+         assertEquals(appregContainer.getTemplate(), "system:/groovy/portal/webui/container/UIContainer.gtmpl");
+         assertEquals("300px", appregContainer.getWidth());
+         assertEquals("400px", appregContainer.getHeight());
+         assertEquals("Everyone", Utils.join(";", appregContainer.getAccessPermissions()));
+         {
+            // Verify app registry application
+            assertNotNull(appregContainer.getChildren());
+            assertEquals(1, appregContainer.getChildren().size());
+            ComponentData appregComponent = appregContainer.getChildren().get(0);
+            assertTrue(appregComponent instanceof ApplicationData);
+            @SuppressWarnings("unchecked")
+            ApplicationData<Portlet> application = (ApplicationData<Portlet>) appregComponent;
+            assertTrue(application.getType() == ApplicationType.PORTLET);
+            ApplicationState<Portlet> state = application.getState();
+            assertNotNull(state);
+            assertTrue(state instanceof TransientApplicationState);
+            TransientApplicationState<Portlet> tas = (TransientApplicationState<Portlet>) state;
+            assertEquals("exoadmin/ApplicationRegistryPortlet", tas.getContentId());
+            assertNull(tas.getContentState());
+
+            assertEquals("Default:DefaultTheme::Mac:MacTheme::Vista:VistaTheme", application.getTheme());
+            assertEquals("Application Registry", application.getTitle());
+            assertEquals("*:/platform/administrators;*:/organization/management/executive-board", Utils.join(";", application.getAccessPermissions()));
+            assertFalse(application.isShowInfoBar());
+            assertTrue(application.isShowApplicationState());
+            assertFalse(application.isShowApplicationMode());
+            assertEquals("Application Registry", application.getDescription());
+            assertEquals("PortletIcon", application.getIcon());
+            assertEquals("250px", application.getWidth());
+            assertEquals("350px", application.getHeight());
+         }
+
+         // Verify column 2 of container 2
+         ComponentData orgComp = container2.getChildren().get(1);
+         assertTrue(orgComp instanceof ContainerData);
+         ContainerData orgContainer = (ContainerData) orgComp;
+         assertEquals(orgContainer.getTemplate(), "system:/groovy/portal/webui/container/UIContainer.gtmpl");
+         assertEquals("200px", orgContainer.getWidth());
+         assertEquals("300px", orgContainer.getHeight());
+         assertEquals("/platform/users", Utils.join(";", orgContainer.getAccessPermissions()));
+         {
+            // Verify org mgmt application
+            assertNotNull(orgContainer.getChildren());
+            assertEquals(1, orgContainer.getChildren().size());
+            ComponentData orgappComp = orgContainer.getChildren().get(0);
+            assertTrue(orgappComp instanceof ApplicationData);
+            @SuppressWarnings("unchecked")
+            ApplicationData<Portlet> application = (ApplicationData<Portlet>) orgappComp;
+            assertTrue(application.getType() == ApplicationType.PORTLET);
+            ApplicationState<Portlet> state = application.getState();
+            assertNotNull(state);
+            assertTrue(state instanceof TransientApplicationState);
+            TransientApplicationState<Portlet> tas = (TransientApplicationState<Portlet>) state;
+            assertEquals("exoadmin/OrganizationPortlet", tas.getContentId());
+            assertNull(tas.getContentState());
+
+            assertEquals("Vista:VistaTheme::Mac:MacTheme::Default:DefaultTheme", application.getTheme());
+            assertEquals("Organization Management", application.getTitle());
+            assertEquals("*:/platform/administrators;*:/organization/management/executive-board", Utils.join(";", application.getAccessPermissions()));
+            assertTrue(application.isShowInfoBar());
+            assertFalse(application.isShowApplicationState());
+            assertFalse(application.isShowApplicationMode());
+            assertEquals("Organization Management", application.getDescription());
+            assertEquals("StarAwardIcon", application.getIcon());
+            assertEquals("100px", application.getWidth());
+            assertEquals("200px", application.getHeight());
+         }
+      }
+
+      // Verify container 3
+      ComponentData c3 = rootChildren.get(2);
+      assertNotNull(c3);
+      assertTrue(c3 instanceof ContainerData);
+      ContainerData container3 = (ContainerData) c3;
+      assertEquals(container3.getTemplate(), "system:/groovy/portal/webui/container/UIContainer.gtmpl");
+      assertEquals("Everyone", Utils.join(";", container3.getAccessPermissions()));
+      assertNull(container3.getFactoryId());
+      {
+         // Verify site map application
+         assertNotNull(container3.getChildren());
+         assertEquals(1, container3.getChildren().size());
+         ComponentData sitemapcomponent = container3.getChildren().get(0);
+         assertTrue(sitemapcomponent instanceof ApplicationData);
+         @SuppressWarnings("unchecked")
+         ApplicationData<Portlet> application = (ApplicationData<Portlet>) sitemapcomponent;
+         assertTrue(application.getType() == ApplicationType.PORTLET);
+         ApplicationState<Portlet> state = application.getState();
+         assertNotNull(state);
+         assertTrue(state instanceof TransientApplicationState);
+         TransientApplicationState<Portlet> tas = (TransientApplicationState<Portlet>) state;
+         assertEquals("web/SiteMapPortlet", tas.getContentId());
+         assertNull(tas.getContentState());
+
+         assertEquals("Default:DefaultTheme::Vista:VistaTheme::Mac:MacTheme", application.getTheme());
+         assertEquals("SiteMap", application.getTitle());
+         assertEquals("*:/platform/users", Utils.join(";", application.getAccessPermissions()));
+         assertTrue(application.isShowInfoBar());
+         assertTrue(application.isShowApplicationState());
+         assertFalse(application.isShowApplicationMode());
+         assertEquals("SiteMap", application.getDescription());
+         assertNull(application.getIcon());
+         assertNull(application.getWidth());
+         assertNull(application.getHeight());
+      }
+
    }
 }
